48ad069e051849a3d3ca94e0c64b83c1e4d3bca7
Adriana Jimenez <ajimenez0024@gmail.com>
2019-08-11 13:25:23 -0400

    Updates

---
+++ precache-manifest.704c75fba4d449580033bd512db640a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8e52c42d481d33e1e9f9d0d1ccd322",
+    "url": "/palindrome/index.html"
+  },
+  {
+    "revision": "78ddf329a5f9674f721b",
+    "url": "/palindrome/static/css/main.ef115fb7.chunk.css"
+  },
+  {
+    "revision": "99f995443662fa9fcfcd",
+    "url": "/palindrome/static/js/2.953543a4.chunk.js"
+  },
+  {
+    "revision": "78ddf329a5f9674f721b",
+    "url": "/palindrome/static/js/main.d70217e4.chunk.js"
+  },
+  {
+    "revision": "09fbbd793702cc248b68",
+    "url": "/palindrome/static/js/runtime~main.5b2b771b.js"
+  },
+  {
+    "revision": "2e2c5abe4e73b12c009ef43b07ac7e87",
+    "url": "/palindrome/static/media/tacocat.2e2c5abe.svg"
+  }
+]);
